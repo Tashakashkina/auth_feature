0aa77c003bd17c055083e55558af067c09f4c7ec
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-09-20 15:36:48 +0400

    remove widget test (entering_phone_page_test.dart)

--- test/feature/presentation/pages/entering_phone_page_test.dart
+++ test/feature/presentation/pages/entering_phone_page_test.dart
@@ -1,18 +1,1 @@
-import 'package:auth_feature/feature/presentation/pages/entering_phone_page.dart';
-import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  group('Entering phone page UI', (() {
-    testWidgets(
-        'The home page is open'
-        'There should be a textfield for entering phone'
-        'And there should be a button to continue',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(const EnteringPhonePage());
-      await tester.pump();
-
-      expect(find.byKey(EnteringPhonePage.phoneInputKey), findsOneWidget);
-      expect(find.byKey(EnteringPhonePage.letsStartButtonKey), findsOneWidget);
-    });
-  }));
-}
