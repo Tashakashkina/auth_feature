350acb2ae1bf5499b2eb544640cb38676aa47307
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-10-12 21:17:57 +0400

    add bloc provider  to check if the token exists

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
+import 'package:auth_feature/feature/presentation/bloc/bloc/token/auth_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'feature/data/datasources/auth_feature_remote_data_source.dart';
 import 'feature/presentation/pages/entering_otp_page.dart';
 import 'feature/presentation/pages/entering_phone_page.dart';
 import 'feature/presentation/pages/success_auth_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'injection_container.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -44,27 +45,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
-      theme: ThemeData(shadowColor: const Color.fromRGBO(243, 243, 243, 1)),
-      debugShowCheckedModeBanner: false,
+    return BlocProvider(
+      create: (context) => sl<AuthBloc>()..add(CheckAuthToken()),
+      lazy: false,
+      child: MaterialApp.router(
+        routerConfig: _router,
+        theme: ThemeData(shadowColor: const Color.fromRGBO(243, 243, 243, 1)),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
-
-// class BlocNavigate extends StatelessWidget {
-//   const BlocNavigate({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocBuilder<AuthenticationBloc, AuthenticationState>(
-//       builder: (context, state) {
-//         if (state is AuthenticationSuccess) {
-//           return const SuccessAuthPage();
-//         } else {
-//           return const EnteringPhonePage();
-//         }
-//       },
-//     );
-  
-
