e2ebac6f7b191fa479a05f3fe84f7692f7fdef44
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-10-12 21:13:07 +0400

    add injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,8 +1,46 @@
+import 'package:auth_feature/feature/data/datasources/auth_feature_local_data_source.dart';
 import 'package:auth_feature/feature/domain/repositories/auth_feature_repository.dart';
+import 'package:auth_feature/feature/domain/usecases/restore_token.dart';
+import 'package:auth_feature/feature/network/network_info.dart';
+import 'package:auth_feature/feature/presentation/bloc/bloc/token/auth_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'feature/data/datasources/auth_feature_remote_data_source.dart';
 import 'feature/data/repositories/auth_feature_repository_impl.dart';
 import 'feature/domain/usecases/get_token.dart';
 
 GetIt sl = GetIt.instance;
-Future<void> init() async {}
+Future<void> init() async {
+  sl.registerLazySingleton(() {
+    return AuthBloc(
+      restoreToken: sl(),
+    );
+  });
+
+  sl.registerLazySingleton(() => RestoreToken(sl()));
+
+  sl.registerLazySingleton<AuthFeatureRepository>(
+    () => AuthFeatureRepositoryImpl(
+        localDataSource: sl(), remoteDataSource: sl(), networkInfo: sl()),
+  );
+
+  sl.registerLazySingleton<AuthFeatureRemoteDataSource>(
+      () => AuthFeatureRemoteDataSourceImpl(
+            instance: sl(),
+          ));
+
+  sl.registerLazySingleton<AuthFeatureLocalDataSource>(
+      () => AuthFeatureLocalDataSourceImpl(
+            sharedPreferences: sl(),
+          ));
+  sl.registerLazySingleton(() => FirebaseAuth.instance);
+
+  final sharedPreferences = SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
