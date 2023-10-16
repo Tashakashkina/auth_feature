392bb22effb89387f1e6e4d16052be9bddcbc748
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-10-17 01:34:16 +0400

    add BlocNavigate page (main.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,51 @@
 import 'package:auth_feature/feature/presentation/bloc/bloc/token/auth_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'feature/presentation/pages/entering_otp_page.dart';
 import 'feature/presentation/pages/entering_phone_page.dart';
 import 'feature/presentation/pages/success_auth_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'injection_container.dart';
+import 'injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
 
-User? result = FirebaseAuth.instance.currentUser;
 final GoRouter _router = GoRouter(initialLocation: '/', routes: <RouteBase>[
   GoRoute(
-    path: '/',
-    builder: (BuildContext context, GoRouterState state) =>
-        const EnteringPhonePage(),
-    routes: <RouteBase>[
-      GoRoute(
-        path: 'otp',
-        builder: (BuildContext context, GoRouterState state) =>
-            const EnteringOtpPage(),
-        routes: <RouteBase>[
-          GoRoute(
-              path: 'success',
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) =>
+          const BlocNavigate(),
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'phone',
+          builder: (BuildContext context, GoRouterState state) =>
+              const EnteringPhonePage(),
+          routes: <RouteBase>[
+            GoRoute(
+              path: 'otp',
               builder: (BuildContext context, GoRouterState state) =>
-                  const SuccessAuthPage()),
-        ],
-      ),
-    ],
-  )
+                  const EnteringOtpPage(),
+              routes: <RouteBase>[
+                GoRoute(
+                    path: 'success',
+                    builder: (BuildContext context, GoRouterState state) =>
+                        SuccessAuthPage()),
+              ],
+            ),
+          ],
+        )
+      ])
 ]);
 
 class MyApp extends StatelessWidget {
@@ -52,6 +60,32 @@ class MyApp extends StatelessWidget {
         routerConfig: _router,
         theme: ThemeData(shadowColor: const Color.fromRGBO(243, 243, 243, 1)),
         debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
+
+class BlocNavigate extends StatefulWidget {
+  const BlocNavigate({Key? key}) : super(key: key);
+
+  @override
+  State<BlocNavigate> createState() => _BlocNavigateState();
+}
+
+class _BlocNavigateState extends State<BlocNavigate> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthCompleted) {
+          context.go('/otp/phone/success');
+        } else {
+          context.go('/phone');
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.blue[100],
+        body: Center(child: CircularProgressIndicator()),
       ),
     );
   }
