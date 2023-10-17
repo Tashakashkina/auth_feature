02bf9909add3ea34f30a74adc7e5db6d2f1250ae
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-10-17 17:24:43 +0400

    add _getToken method to bloc

--- lib/feature/presentation/bloc/bloc/token/auth_bloc.dart
+++ lib/feature/presentation/bloc/bloc/token/auth_bloc.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
-
 import 'package:auth_feature/core/usecase.dart';
-import 'package:auth_feature/feature/domain/repositories/auth_feature_repository.dart';
 import 'package:auth_feature/feature/domain/usecases/clear_storage.dart';
+import 'package:auth_feature/feature/domain/usecases/get_token.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import '../../../../domain/usecases/restore_token.dart';
 
 part 'auth_event.dart';
@@ -14,12 +12,16 @@ part 'auth_state.dart';
 class AuthBloc extends Bloc<AuthTokenEvent, AuthState> {
   final RestoreToken restoreToken;
   final ClearStorage clearStorage;
+  final GetToken getAuthToken;
 
-  AuthBloc({required this.clearStorage, required this.restoreToken})
+  AuthBloc(
+      {required this.getAuthToken,
+      required this.clearStorage,
+      required this.restoreToken})
       : super(AuthChecking()) {
     on<CheckAuthToken>(_checkIfAuth);
     on<ClearTokenStorage>(_clearToken);
-    // on<GetTokenFromFirebase>(_getToken);
+    on<GetTokenFromFirebase>(_getToken);
   }
 
   Future<void> _checkIfAuth(
@@ -36,5 +38,13 @@ class AuthBloc extends Bloc<AuthTokenEvent, AuthState> {
     final result = await clearStorage.call(NoParams());
     result.fold(
         (l) => emit(const AuthError('error')), (r) => emit(AuthClearStorage()));
+  }
+
+  Future<void> _getToken<GetTokenFromFirebase>(
+      GetTokenFromFirebase event, Emitter<AuthState> emit) async {
+    emit(AuthGetToken());
+    final result = await getAuthToken.call(NoParams());
+    result.fold(
+        (l) => emit(const AuthError('error')), (r) => emit(AuthCompleted()));
   }
 }
