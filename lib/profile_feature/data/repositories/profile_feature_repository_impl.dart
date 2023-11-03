24b3a0d05925796a738594aa6e535bea5e4b3076
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-11-03 22:50:29 +0400

    add repository impl with the passed test

---
+++ lib/profile_feature/data/repositories/profile_feature_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:auth_feature/core/error/failures.dart';
+import 'package:auth_feature/feature/network/network_info.dart';
+import 'package:auth_feature/profile_feature/data/datasources/profile_feature_remote_datasource.dart';
+import 'package:auth_feature/profile_feature/domain/entities/user.dart';
+import 'package:auth_feature/profile_feature/domain/repositories/profile_feature_repository.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../core/error/exceptions.dart';
+
+class ProfileFeatureRepositoryImpl implements ProfileFeatureRepository {
+  final ProfileFeatureRemoteDataSource remoteDataSource;
+  final NetworkInfo networkInfo;
+
+  ProfileFeatureRepositoryImpl(
+      {required this.remoteDataSource, required this.networkInfo});
+
+  @override
+  Future<Either<Failure, User>> getUserData() async {
+    if (await networkInfo.isConnected) {
+      try {
+        final result = await remoteDataSource.getUserData();
+        return Right(result!);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(ServerFailure());
+    }
+  }
+}
--- test/profile_feature/data/repositories/profile_feature_repository_impl_test.dart
+++ test/profile_feature/data/repositories/profile_feature_repository_impl_test.dart
@@ -1,36 +1,85 @@
+import 'package:auth_feature/core/error/exceptions.dart';
 import 'package:auth_feature/core/error/failures.dart';
-import 'package:auth_feature/core/usecase.dart';
+import 'package:auth_feature/feature/network/network_info.dart';
+import 'package:auth_feature/profile_feature/data/datasources/profile_feature_remote_datasource.dart';
+import 'package:auth_feature/profile_feature/data/models/profile_feature_model.dart';
+import 'package:auth_feature/profile_feature/data/repositories/profile_feature_repository_impl.dart';
 import 'package:auth_feature/profile_feature/domain/entities/user.dart';
-import 'package:auth_feature/profile_feature/domain/repositories/profile_feature_repository.dart';
-import 'package:auth_feature/profile_feature/domain/usecases/get_user_data.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockProfileFeatureRepository extends Mock
-    implements ProfileFeatureRepository {}
+class MockRemoteDataSource extends Mock
+    implements ProfileFeatureRemoteDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
 
 void main() {
-  late ProfileFeatureRepository mockProfileFeatureRepository;
-  late GetUserDataUsecase usecase;
+  late ProfileFeatureRepositoryImpl profileFeatureRepositoryImpl;
+  late MockRemoteDataSource mockRemoteDataSource;
+  late MockNetworkInfo mockNetworkInfo;
+  final tUserModel = UserModel(
+    uid: '1234',
+    email: 'email',
+    time: DateTime.now(),
+  );
+  User tUser = tUserModel;
   setUp(() {
-    mockProfileFeatureRepository = MockProfileFeatureRepository();
-    usecase = GetUserDataUsecase(repository: mockProfileFeatureRepository);
+    mockRemoteDataSource = MockRemoteDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+    profileFeatureRepositoryImpl = ProfileFeatureRepositoryImpl(
+      networkInfo: mockNetworkInfo,
+      remoteDataSource: mockRemoteDataSource,
+    );
   });
-  const tUser = User(uid: '1234', email: 'email', time: 1);
-  test("should call the getUserData repo method", () async {
-    //arrange
-
-    Either<Failure, User> repoResponse = const Right(tUser);
-    when(() => mockProfileFeatureRepository.getUserData())
-        .thenAnswer((_) async => repoResponse);
-
-    //act
-    final result = await usecase(NoParams());
-
-    //assert
-    expect(result, equals(repoResponse));
-    verify(() => mockProfileFeatureRepository.getUserData());
-    verifyNoMoreInteractions(mockProfileFeatureRepository);
+  group('get user data', () {
+    test('should check if device is online', () async {
+      when(
+        () => mockNetworkInfo.isConnected,
+      ).thenAnswer((_) async => true);
+      await mockNetworkInfo.isConnected;
+      verify(() => mockNetworkInfo.isConnected);
+    });
+    group('when device is online', () {
+      setUp(() {
+        when(
+          () => mockNetworkInfo.isConnected,
+        ).thenAnswer((_) async => true);
+      });
+      test('should return remote data when call is succesfull', () async {
+        when(
+          () => mockRemoteDataSource.getUserData(),
+        ).thenAnswer((_) async => tUserModel);
+        final result = await profileFeatureRepositoryImpl.getUserData();
+        verify(
+          () => mockRemoteDataSource.getUserData(),
+        );
+        expect(result, equals(Right(tUser)));
+      });
+      test('should return failure when call is unsuccesfull', () async {
+        when(
+          () => mockRemoteDataSource.getUserData(),
+        ).thenThrow(ServerException());
+        final result = await profileFeatureRepositoryImpl.getUserData();
+        verify(
+          () => mockRemoteDataSource.getUserData(),
+        );
+        expect(result, equals(Left(ServerFailure())));
+      });
+    });
+    group('device is offline', () {
+      setUp(() {
+        when(
+          () => mockNetworkInfo.isConnected,
+        ).thenAnswer((_) async => false);
+      });
+      test('should return failure', () async {
+        final result = await profileFeatureRepositoryImpl.getUserData();
+        verifyNever(
+          () => mockRemoteDataSource.getUserData(),
+        );
+        expect(result, equals(Left(ServerFailure())));
+      });
+    });
   });
 }
