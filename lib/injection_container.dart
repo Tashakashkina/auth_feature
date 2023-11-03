df6e690cd3a74493e3ded9f685c3ddb6bf0411c6
Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-11-03 22:47:15 +0400

    fix injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -4,6 +4,11 @@ import 'package:auth_feature/feature/domain/usecases/clear_storage.dart';
 import 'package:auth_feature/feature/domain/usecases/restore_token.dart';
 import 'package:auth_feature/feature/network/network_info.dart';
 import 'package:auth_feature/feature/presentation/bloc/bloc/token/auth_bloc.dart';
+import 'package:auth_feature/profile_feature/data/datasources/profile_feature_remote_datasource.dart';
+import 'package:auth_feature/profile_feature/data/repositories/profile_feature_repository_impl.dart';
+import 'package:auth_feature/profile_feature/domain/repositories/profile_feature_repository.dart';
+import 'package:auth_feature/profile_feature/domain/usecases/get_user_data.dart';
+import 'package:auth_feature/profile_feature/presentation/bloc/profile_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -20,31 +25,43 @@ Future<void> init() async {
         clearStorage: sl(),
         getAuthToken: sl(),
       ));
+  sl.registerLazySingleton(() => ProfileBloc(getUserData: sl()));
+//
 
   sl.registerLazySingleton(() => RestoreToken(sl()));
   sl.registerLazySingleton(() => ClearStorage(sl()));
   sl.registerLazySingleton(() => GetToken(sl()));
-
+  sl.registerLazySingleton(() => GetUserDataUsecase(repository: sl()));
+//
   sl.registerLazySingleton<AuthFeatureRepository>(
     () => AuthFeatureRepositoryImpl(
         localDataSource: sl(), remoteDataSource: sl(), networkInfo: sl()),
   );
 
+  sl.registerLazySingleton<ProfileFeatureRepository>(
+    () =>
+        ProfileFeatureRepositoryImpl(remoteDataSource: sl(), networkInfo: sl()),
+  );
+//
   sl.registerLazySingleton<AuthFeatureRemoteDataSource>(
       () => AuthFeatureRemoteDataSourceImpl(
             instance: sl(),
           ));
 
+  sl.registerLazySingleton<ProfileFeatureRemoteDataSource>(
+      () => ProfileFeatureRemoteDataSourceImpl(auth: sl(), firestore: sl()));
+//
   sl.registerLazySingleton<AuthFeatureLocalDataSource>(
       () => AuthFeatureLocalDataSourceImpl(
             sharedPreferences: sl(),
           ));
+  //
   sl.registerLazySingleton(() => FirebaseAuth.instance);
-
+//
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => sharedPreferences);
-
+//
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
-
+//
   sl.registerLazySingleton(() => InternetConnectionChecker());
 }
